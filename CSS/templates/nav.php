f0aa5e85ebb40c5f37964dfb0c9680b5e7883651
mbotejoy <joy.mbote@strathmore.edu>
2024-06-10 13:36:11 +0300

    reusability of code

---
+++ CSS/templates/nav.php
@@ -0,0 +1,15 @@
+<div class="topnav">
+            <!--Links to go back to the home page-->
+    <a href="index.html">Home</a>
+    <a href="mpesa.html" >Mpesa</a>
+    <a href="Customer Care.html" >Customer Care</a>
+    <a href="shop.html" >Safaricom Shop</a>
+    <a href="contact.html" >Contact us</a>
+    <a href="about.html">About</a>
+    <a href="help.html" >Help</a>
+
+    <div class ="topnav-Right">
+        <a href = "signup.html">Sign Up</a>
+        <a href="signin.html">Sign In</a>
+    </div>
+    </div>
