c898962933da8423d30e8ae6a3422a8e67db7446
mbotejoy <joy.mbote@strathmore.edu>
2024-06-10 13:41:34 +0300

    changed customer care.html to php file

--- Customer Care.php
+++ Customer Care.php
@@ -15,20 +15,8 @@
     </style>
 </head>
 <body>
-    <div class="topnav">
-        <a href="index.html">Home</a>
-        <a href="mpesa.html" >Mpesa</a>
-        <a href="Customer Care.html" >Customer Care</a>
-        <a href="shop.html" >Safaricom Shop</a>
-        <a href="contact.html" >Contact us</a>
-        <a href="about.html">About</a>
-        <a href="help.html" >Help</a>
+<?php include_once("template/nav.php");?>
 
-            <div class="topnav-Right">
-                <a href="signup.html">Sign Up</a>
-                <a href="signin.html">Sign In</a>
-            </div>
-    </div>
 
     <header>
         <div class="header">
