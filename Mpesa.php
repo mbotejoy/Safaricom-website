5a563cce9cc471ac651f88567e6722a709bbfa35
mbotejoy <joy.mbote@strathmore.edu>
2024-06-10 13:54:59 +0300

    changed to mpesa.php

--- Mpesa.php
+++ Mpesa.php
@@ -17,21 +17,7 @@
 
 </head>
 <body>
-     <div class="topnav">
-                <!--Links to go back to the home page-->
-                <a href="index.html">Home</a>
-                <a href="mpesa.html" >Mpesa</a>
-                <a href="Customer Care.html" >Customer Care</a>
-                <a href="shop.html" >Safaricom Shop</a>
-                <a href="contact.html" >Contact us</a>
-                <a href="about.html">About</a>
-                <a href="help.html" >Help</a>
-    
-        <div class ="topnav-Right">
-            <a href = "signup.html">Sign Up</a>
-            <a href="signin.html">Sign In</a>
-        </div>
-        </div>
+<?php include_once("template/nav.php");?>
 <header>
         <div class="header">
         
