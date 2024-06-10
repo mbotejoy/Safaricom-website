2c5a0c9d68aa4da8756d8480bc3f1276924a2f6f
mbotejoy <joy.mbote@strathmore.edu>
2024-06-10 13:52:42 +0300

    changed to help.php

--- help.php
+++ help.php
@@ -25,23 +25,7 @@
 </head>
 <body>
     <body>
-        <div class="topnav">
-            <!--Links to go back to the home page-->
-    
-    
-            <a href="index.html">Home</a>
-            <a href="mpesa.html" >Mpesa</a>
-            <a href="Customer Care.html" >Customer Care</a>
-            <a href="shop.html" >Safaricom Shop</a>
-            <a href="contact.html" >Contact us</a>
-            <a href="about.html">About</a>
-            <a href="help.html" >Help</a>
-    
-    <div class ="topnav-Right">
-        <a href = "signup.html">Sign Up</a>
-        <a href="signin.html">Sign In</a>
-    </div>
-</div>
+    <?php include_once("template/nav.php");?>
         
         <h1 style="text-align: center;color: green;"> How Can We Help ?</h1>
         <h4 style="text-align: center;text-decoration: dotted;">Have Any Questions? Search through our Help Center</h4>
