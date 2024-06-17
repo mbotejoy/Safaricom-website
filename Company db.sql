85dda379d88ddd06e26b50d841fd5939e3d79420
mbotejoy <joy.mbote@strathmore.edu>
2024-06-17 22:34:25 +0300

    Created Database using the interface procedure

---
+++ Company db.sql
@@ -0,0 +1,104 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 17, 2024 at 09:31 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `company`
+--
+DROP DATABASE IF EXISTS `company`;
+CREATE DATABASE IF NOT EXISTS `company` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `company`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+DROP TABLE IF EXISTS `customers`;
+CREATE TABLE IF NOT EXISTS `customers` (
+  `FullName` varchar(11) NOT NULL,
+  `CustomerId` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`FullName`),
+  UNIQUE KEY `Customerid` (`CustomerId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`FullName`, `CustomerId`, `email`, `password`) VALUES
+('Felix Opio', '7645', 'Opio@yahoo.com', '0710felix'),
+('Kevina Arit', '1798', 'Aritia@gmail.com', 'Kevina0653'),
+('Sharon Aime', '1678', 'sharon.aime@gmail.com', 'sharon789');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employees`
+--
+
+DROP TABLE IF EXISTS `employees`;
+CREATE TABLE IF NOT EXISTS `employees` (
+  `FullName` varchar(11) NOT NULL,
+  `UserId` varchar(50) DEFAULT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`FullName`),
+  UNIQUE KEY `userid` (`UserId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `employees`
+--
+
+INSERT INTO `employees` (`FullName`, `UserId`, `email`, `password`) VALUES
+('Alice Kiman', '6543', 'kimaniA@gmail.com', 'alicok890'),
+('Angela Bara', '9875', 'angelaB@gmail.com', '130Baraza'),
+('Peter James', '7890', 'pJames@yahoo.com', 'James254yahoo');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employers`
+--
+
+DROP TABLE IF EXISTS `employers`;
+CREATE TABLE IF NOT EXISTS `employers` (
+  `userid` bigint(11) NOT NULL,
+  `fullname` varchar(50) DEFAULT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`userid`),
+  UNIQUE KEY `fullname` (`fullname`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `employers`
+--
+
+INSERT INTO `employers` (`userid`, `fullname`, `email`, `password`) VALUES
+(0, 'Judy', 'judy@gmail.com', 'J4dy.000'),
+(1234, 'Harriet', 'haru@yahoo.com', 'Harriet1476');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
