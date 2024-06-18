1d43705a2a006a50a21c7fbcb1e536018932f501
mbotejoy <joy.mbote@strathmore.edu>
2024-06-18 13:05:56 +0300

    Added form to sign up

--- signup.php
+++ signup.php
@@ -15,12 +15,32 @@
             <h1>Signup with Us Today</h1>
         </div>
         <div class="content">
-        <h4>
+          <h4>
            <p>Don't seem to have an account?<br>
            We are excited to have you on board!<br>
            Sign Up today!</p>
 
-        </h4>
+           </h4>
+        </div>
+        <div class="form">
+         <forms action="" class="form">
+            
+      
+        
+        <label for="FN"> Fullname : </label><br>
+        <input type = "text" id= "FN" placeholder=" Enter Fullname"><br><br>
+        <label for="Eaddress">Email Address : </Address></label><br>
+        <input type = "text" id= "Eaddress" placeholder="Enter your Email Address"><br><br>
+        <label for="pswrd">Set Password : </Address></label><br>
+        <input type = "text" id= "Password" placeholder="Set password"><br><br>
+        <label for="pswrd">Confirm Password : </Address></label><br>
+        <input type = "text" id= "Password" placeholder="Confirm password"><br><br>
+
+        <input type="submit" value="Sign Up">
+        
+
+
+        
 
 </body>
 </html>
