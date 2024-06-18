83d996fb1650c264d2796c52541afd216ff0e067
mbotejoy <joy.mbote@strathmore.edu>
2024-06-18 12:47:58 +0300

    Added CSS link to signup.php

--- Mpesa.php
+++ Mpesa.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mpesa</title>
-    <link rel="stylesheet"href="CSS/style.css">
+    <link rel="stylesheet"href="css/style.Css">
     <!--INTERNAL CSS-->
     <style>
         h1,h3{
