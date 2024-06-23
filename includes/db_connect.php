cf9faec88685afc1565b0ce509548486b9ae1f1f
mbotejoy <joy.mbote@strathmore.edu>
2024-06-23 13:27:43 +0300

    wrote the database connection again

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root";
+$username = "username";
 $password = "";
-$db_name="company";
+$db_name = "company";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $db_name);
@@ -11,5 +11,5 @@ $conn = new mysqli($servername, $username, $password, $db_name);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-//echo "Connected successfully";
+echo "Connected successfully";
 ?>
