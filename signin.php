7a3aeda01e9c77a83a745b0c66be1a605ef1cbd1
mbotejoy <joy.mbote@strathmore.edu>
2024-06-23 13:15:18 +0300

    changed name for css

--- signin.php
+++ signin.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet"href="css/style.Css">
+    <link rel="stylesheet"href="css/style.css">
 </head>
 <body>
     <?php include_once("templates/nav.php");?>
@@ -29,6 +29,6 @@
 
            
     
-            <input type="submit" value="Sign In">
+            <input type="submit" value="Sign In" >
 </body>
 </html> 
