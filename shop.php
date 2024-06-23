9c51763db3755d33addf57597498b95c0f599e38
mbotejoy <joy.mbote@strathmore.edu>
2024-06-23 13:13:27 +0300

    changed name for css

--- shop.php
+++ shop.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Safaricom shop</title>
-    <link rel="stylesheet"href="css/style.Css">
+    <link rel="stylesheet"href="css/style.css">
 
 
 
