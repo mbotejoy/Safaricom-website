76f7fd460f0a42efd0aeeb894a6445559e4850ae
mbotejoy <joy.mbote@strathmore.edu>
2024-07-06 15:08:40 +0300

    header template using php

--- signup.php
+++ signup.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SignUp</title>
-    <link rel="stylesheet"href="css/style.css">
-</head>
+<?php include_once("templates/header.php")?>
 <body>
 <?php// link for a navigation bar?>
 <?php include_once("templates/nav.php");?>
