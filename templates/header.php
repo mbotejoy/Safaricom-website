fa7bcec788be0c754df5be5addbc7daf628c7e48
mbotejoy <joy.mbote@strathmore.edu>
2024-07-06 15:17:10 +0300

    Adjusted header template

--- templates/header.php
+++ templates/header.php
@@ -4,5 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Safaricom</title>
+    <!--Link to CSS-->
     <link rel="stylesheet"href="css/style.css">
 </head>    
