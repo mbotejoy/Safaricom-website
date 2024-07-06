14efa294ad97ca92b06c5b9baa087e4971b92de1
mbotejoy <joy.mbote@strathmore.edu>
2024-07-06 15:10:19 +0300

    header template using php

--- shop.php
+++ shop.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Safaricom shop</title>
-    <link rel="stylesheet"href="css/style.css">
-
-
-
-</head>
+<?php include_once("templates/header.php")?>
 <body>
 <?php include_once("templates/nav.php");?>
     
